6853ad13483548fed93c6af7ac7eff78f58b288b
Laércio de Sousa <laercio.sousa@aiscgre.org.br>
2026-04-09 23:52:10 -0300

    Updated project for use with Zed editor.

---
+++ target/debug/deps/find_msvc_tools-ef29dcd9f97b56f5.d
@@ -0,0 +1,9 @@
+/home/lbsou/Documents/aiscgre-br/tree-sitter-gregorio/target/debug/deps/find_msvc_tools-ef29dcd9f97b56f5.d: /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs
+
+/home/lbsou/Documents/aiscgre-br/tree-sitter-gregorio/target/debug/deps/libfind_msvc_tools-ef29dcd9f97b56f5.rlib: /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs
+
+/home/lbsou/Documents/aiscgre-br/tree-sitter-gregorio/target/debug/deps/libfind_msvc_tools-ef29dcd9f97b56f5.rmeta: /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs
+
+/home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs:
+/home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs:
+/home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs:
