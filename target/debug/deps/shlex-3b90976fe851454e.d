53e9a8669d0408c3479d982c57f0c091ed18e792
Laércio de Sousa <laercio.sousa@aiscgre.org.br>
2026-05-02 18:59:40 -0300

    Add target/ to .gitignore and remove tracked build artifacts
    - Add target/ to .gitignore (Cargo/rustc artifacts should not be tracked)
    - Update Cargo.lock: tree-sitter 0.20 -> 0.22
    - Remove all target/ files from git index

--- target/debug/deps/shlex-3b90976fe851454e.d
+++
@@ -1,8 +0,0 @@
-/home/lbsou/Documents/aiscgre-br/tree-sitter-gregorio/target/debug/deps/shlex-3b90976fe851454e.d: /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
-
-/home/lbsou/Documents/aiscgre-br/tree-sitter-gregorio/target/debug/deps/libshlex-3b90976fe851454e.rlib: /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
-
-/home/lbsou/Documents/aiscgre-br/tree-sitter-gregorio/target/debug/deps/libshlex-3b90976fe851454e.rmeta: /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
-
-/home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs:
-/home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs:
--- target/debug/deps/tree_sitter-16647355fe0996d4.d
+++
@@ -1,9 +0,0 @@
-/home/lbsou/Documents/aiscgre-br/tree-sitter-gregorio/target/debug/deps/tree_sitter-16647355fe0996d4.d: /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/lib.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/ffi.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/util.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/./bindings.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/../include/tree_sitter/parser.h
-
-/home/lbsou/Documents/aiscgre-br/tree-sitter-gregorio/target/debug/deps/libtree_sitter-16647355fe0996d4.rmeta: /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/lib.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/ffi.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/util.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/./bindings.rs /home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/../include/tree_sitter/parser.h
-
-/home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/lib.rs:
-/home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/ffi.rs:
-/home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/util.rs:
-/home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/./bindings.rs:
-/home/lbsou/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-0.20.10/binding_rust/../include/tree_sitter/parser.h:
